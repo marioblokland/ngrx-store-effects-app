52cae546b29fcd421a2feade5c8f2e7e07dc23a6
Blokland <mario@capturemedia.ch>
2019-04-07 18:31:02 +0200

    State composition with selectors

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,10 +1,22 @@
 import * as Pizzas from './pizzas.reducers';
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+import * as PizzaReducers from './pizzas.reducers';
 
 export interface ProductsState {
-  pizzas: Pizzas.PizzaState
+  pizzas: Pizzas.PizzaState,
 }
 
 export const reducers: ActionReducerMap<ProductsState> = {
   pizzas: Pizzas.reducer,
 };
+
+export const getProductsState = createFeatureSelector<ProductsState>('products');
+
+export const getPizzaState = createSelector(
+  getProductsState,
+  (state: ProductsState) => state.pizzas
+);
+
+export const getAllPizzas = createSelector(getPizzaState, PizzaReducers.getPizzas);
+export const getPizzasLoaded = createSelector(getPizzaState, PizzaReducers.getPizzasLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, PizzaReducers.getPizzasLoading);
