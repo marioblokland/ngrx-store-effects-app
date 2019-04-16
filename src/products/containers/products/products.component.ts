3a8c256db188539ed687f94ca99af574de2e91c9
Blokland <mario@capturemedia.ch>
2019-04-16 13:44:24 +0200

    Visualise toppings

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
@@ -35,5 +35,6 @@ export class ProductsComponent implements OnInit {
   ngOnInit() {
     this.pizzas$ = this.store.select(fromStore.getAllPizzas);
     this.store.dispatch(new fromStore.LoadPizzas());
+    this.store.dispatch(new fromStore.LoadToppings());
   }
 }
