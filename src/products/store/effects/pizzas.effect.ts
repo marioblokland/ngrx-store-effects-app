912f334f887b1c4c3c6cbb97a25a95ef467423c4
Blokland <mario@capturemedia.ch>
2019-04-16 14:45:13 +0200

    Made it able to create a new Pizza

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -12,7 +12,8 @@ export class PizzasEffects {
   constructor(
     private actions$: Actions,
     private pizzaService: fromServices.PizzasService
-  ) {}
+  ) {
+  }
 
   @Effect()
   loadPizzas$ = this.actions$.ofType(pizzaActions.LOAD_PIZZAS).pipe(
@@ -23,6 +24,17 @@ export class PizzasEffects {
           map(pizzas => new pizzaActions.LoadPizzasSuccess(pizzas)),
           catchError(error => of(new pizzaActions.LoadPizzasFail(error)))
         );
+    })
+  );
+
+  @Effect()
+  createPizza$ = this.actions$.ofType(pizzaActions.CREATE_PIZZA).pipe(
+    map((action: pizzaActions.CreatePizza) => action.payload),
+    switchMap(pizza => {
+      return this.pizzaService.createPizza(pizza).pipe(
+        map(pizza => new pizzaActions.CreatePizzaSuccess(pizza)),
+        catchError(error => of(new pizzaActions.CreatePizzaFail(error)))
+      );
     })
   );
 }
--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -55,6 +55,16 @@ export function reducer(
         loaded: false,
       };
     }
+
+    case fromPizzas.CREATE_PIZZA_SUCCESS: {
+      const pizza = action.payload;
+      const entities = {...state.entities, [pizza.id]: pizza};
+
+      return {
+        ...state,
+        entities
+      };
+    }
   }
 
   return state;
